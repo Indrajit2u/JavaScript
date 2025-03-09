bfc96fc0601502dd23944191357f57e6cfc7b3e7
Indrajit Behera <indrajitmail06@gmail.com>
2025-03-09 12:54:05 +0530

    conversion f data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+let score2 = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+
+let boolenIsLoggedin = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
